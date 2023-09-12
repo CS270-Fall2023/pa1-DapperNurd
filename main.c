5ffaf0f2d4aca0cbae6ba629a93e2954d3d2f57c
Ben Bonus <jbenbonus@gmail.com>
2023-09-12 10:23:53 -0700

    Fixed memory issues

--- main.c
+++ main.c
@@ -7,19 +7,21 @@
  // ^ includes <stdio.h>, <stdlib.h>, and <string.h>
 
 int main() {
+    int STRING_LENGTH = 256; // Maximum size of input string
 
     // String input gathering
     printf("Please, enter the string: ");
-    char **arrayOfTokens, str[56]; // For some reason any number greater that 56 is a segmentation fault; I do not know why.
-    fgets(str, 56, stdin); 
+    char str[STRING_LENGTH]; // Creates array for the initial string, of size STRING_LENGTH
+    fgets(str, STRING_LENGTH, stdin);  // Gets the input
 
     // getToken handling
-    int numberOfToken;
+    int numberOfToken; // Number of individual words in the string
+    char **arrayOfTokens; // Array of pointers to char*
     numberOfToken = getTokens(str, &arrayOfTokens);
 
     // Displaying tokens
     printf("Number of Tokens: %i\n", numberOfToken);
-    for(int i = 0; i < numberOfToken+1; i++) { // Runs number+1 to display NULL at the end
+    for(int i = 0; i <= numberOfToken; i++) { // Runs with <= to display all tokens plus NULL at the end
         printf("Token %i: %s\n", i+1, arrayOfTokens[i]);
     }
     
--- pa1.c
+++ pa1.c
@@ -7,11 +7,16 @@
  // ^ includes <stdio.h>, <stdlib.h>, and <string.h>
 
 int getTokens(char* s, char*** args) {
-    int count = 0, startIndex = 0, endIndex = 0, strLength = strlen(s);
+    int count = 0, startIndex = 0, endIndex = 0;
+    int length = strlen(s);
 
-    s[strLength-1] = '\0'; // Gets rid of new line at the end
+    *args = malloc(sizeof(char**)); // Allocates space for array of pointers itself
 
-    for(int i = 0; i <= strlen(s); i++) {
+    **args = malloc(sizeof(char*) * length/2 + 2); // Allocates space for each pointer in the array... uses the maximum possible word count as size, assuming every other word is a space (plus one to account for rounding down, and another plus one for NULL at the end)
+
+    s[length-1] = '\0'; // Gets rid of new line at the end
+
+    for(int i = 0; i <= length; i++) {
         if(s[i] == ' ' || s[i] == '\0') { // Tests if current word is space (or end of string)
             int wordLength = endIndex - startIndex; // Length of current word
             if(wordLength != 0) { // Skip if length is zero (repeating spaces)
